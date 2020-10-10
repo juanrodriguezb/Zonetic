39006cb42697895ae97ac2eeb2062f37ba172623
juanrodriguezb <68094743+juanrodriguezb@users.noreply.github.com>
2020-10-09 23:26:54 -0500

    Add files via upload

---
+++ gps_web/src/App.js
@@ -0,0 +1,318 @@
+import React, { Component} from 'react';
+import {GoogleMap,Marker, LoadScript, Polyline} from "@react-google-maps/api";
+import mapStyles from "./mapStyles";
+import axios from 'axios';
+import { Button } from 'rebass';
+import DateTimePicker from 'react-datetime-picker';
+import Switch from "react-switch";
+import SlidingPanel from 'react-sliding-side-panel';
+import Select from 'react-select';
+
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBAt6hp7Ewgy6VWck0sYTxsMOHoWftsh3Y&callback=initMap"
+type="text/javascript"></script>
+
+const API_KEY=process.env.REACT_APP_MAPS_API;
+
+const API_URL_1=process.env.REACT_APP_API_URL_1;
+
+const API_URL_2=process.env.REACT_APP_API_URL_2;
+
+const API_URL_3=process.env.REACT_APP_API_URL_3;
+
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+
+const center={
+  lat:10.9878,
+  lng:-74.7889 ,
+};
+
+const options={
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl:true,
+  streetViewControl: true,
+};
+
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      coord:{
+        lat:10.9878,
+        lng:-74.7889 }
+      ,
+      lat:8.75,
+      lng:-75.883,
+      coord_text:{lng:"XXXX",lat:"XXXX",alt:"XXXX",time:"0000"},
+      sw_realtime:true,
+      sw_history:false,
+      history:[],
+      date_in:new Date(),
+      date_fin:new Date(),
+      openPanel:false,
+      ID:{value:"1",label:"Camion 1"},
+      Opt:[],
+      sw_his_tag:false,
+      sw_tag:true
+    }
+
+  }
+
+  callAPI_users(){
+    axios.get(API_URL_3)
+      .then((res)=>{
+        for (var i=0; i<((res.data).length); i++){
+          this.state.Opt.push({value:(((res.data)[i]).truck), label:("Camion ").concat((((res.data)[i]).truck).toString())})
+        } 
+      });
+  }
+
+  callAPI_actual(){
+
+    axios.post(API_URL_1,({
+      ID:((this.state.ID).value)
+    }))
+      .then((res) => {
+        console.log(res.data);
+        try{
+          this.setState({
+            coord:{
+              lat:res.data[0].lat,
+              lng:res.data[0].lng
+            }
+          });
+
+          var buff_lat=(res.data[0].lat).toString();
+          var buff_lng=(res.data[0].lng).toString();
+          var buff_time=(res.data[0].timegps).toString();
+          var buff_alt=(res.data[0].alt).toString();
+
+          this.setState({
+            coord_text:{lng:buff_lng,lat:buff_lat,alt:buff_alt,time:buff_time}
+          });
+        }catch(error){
+          console.log(Error);
+        };
+      });
+  }
+  
+  callAPI_history(){
+    
+    axios.post(API_URL_2,({
+      timestamp_in:(this.state.date_in).getTime(),
+      timestamp_fin:(this.state.date_fin).getTime(),
+      ID:((this.state.ID).value)
+    }))
+
+      .then((res) => { 
+        var buff = (res.data); 
+        this.setState({
+          history:buff
+        });
+      });
+    
+  }
+
+  set_timer1(){
+    this.timer1 = setInterval(()=>{this.callAPI_actual()}, 1000);
+  }
+
+  set_timer2(){
+    this.timer2 = setInterval(()=>{this.callAPI_history()},1000);
+  }
+
+  componentDidMount(){
+    console.log("Al components mounted");
+    this.callAPI_users();
+    this.set_timer1();
+  }
+
+  render(){return (
+    
+    <div>
+
+    <title>GPS<span role="img" aria-label="Satellite Antenna">📡</span></title>
+
+    <div style={{position:'absolute',top:'50%',left:'-1%',zIndex:'15'}}>
+      <Button onClick={()=>{this.setState({openPanel:!this.state.openPanel})}} style={{color:'black',background:'white'}}>
+      <span role="img" aria-label="Right arrow"> ➡️</span></Button>
+    </div>
+
+    <div style={{position:"absolute",top:"1%",left:"1%",width:"30%",height:"25%",backgroundColor:"white",zIndex:5}}>
+    </div>
+
+    <div>
+    <h1 style={{top:"2%",left:"2%"}}>
+      ZONETIC <span role="img" aria-label="Satellite Antenna">📡</span>
+    </h1>
+    <h4 style={{top:"10%",left:"2%"}}>
+    <body>{"Latitude: "+this.state.coord_text.lat}</body>
+    </h4>
+    <h4 style={{top:"13.5%",left:"2%"}}>
+    <body>{"Longitude: "+this.state.coord_text.lng}</body>
+    </h4>
+    <h4 style={{top:"17%",left:"2%"}}>
+    <body>{"Altitude: "+this.state.coord_text.alt}</body>
+    </h4>
+    <h4 style={{top:"20.5%",left:"2%"}}>
+    <body>{"Timestamp: "+(((new Date(parseFloat(this.state.coord_text.time,10))) + "").split("("))[0]}</body>
+    </h4>
+    </div>
+
+    <SlidingPanel
+      type={'left'}
+      isOpen={this.state.openPanel}
+      size={30}
+      style={{zIndex:7}}
+    >
+
+    <div style={{position:'absolute',top:"50%",left:"31%",zIndex:"15"}}>
+      <Button onClick={()=>{this.setState({openPanel:!this.state.openPanel})}} style={{color:'black',background:'white'}}>
+      <span role="img" aria-label="Left arrow">⬅️</span>
+      </Button>
+    </div>
+
+    <div style={{position:'absolute',top:'0px',left:'0px',zIndex:'5',width:'32%',height:'100%',backgroundColor:'white'}}>
+    </div>
+
+    <h5 style={{top:"5%",left:"2%",zIndex:"10",position:"absolute"}}>Real time position:</h5>
+
+    <div style={{position:'absolute',top:"7.5%",left:"11.5%",zIndex:"10"}}>
+    <Switch
+          checked={this.state.sw_realtime}
+          onChange={(checked)=>{
+            if(checked){
+              this.set_timer1();
+              this.setState({
+                sw_tag:true
+              });
+            }else{
+              clearInterval(this.timer1);
+              this.setState({
+                coord_text:{lng:"XXXX",lat:"XXXX",alt:"XXXX",time:"0000"},
+                sw_tag:false
+              });
+            }
+            this.setState({
+              sw_realtime:checked
+            });
+          }}
+    />
+    </div>
+
+    <h5 style={{top:"10%",left:"2%",zIndex:"10",position:"absolute"}}>View Timeline:</h5>
+
+    <div style={{position:"absolute",top:"12.5%",left:"11.5%",zIndex:"10"}}>
+    <Switch
+          checked={this.state.sw_history}
+          onChange={(checked)=>{
+            if(checked){
+              this.set_timer2();
+              this.setState({
+                sw_his_tag:true
+              });
+            }else{
+              clearInterval(this.timer2);
+              this.setState({
+                sw_his_tag:false
+              });
+            }
+            this.setState({
+              sw_history:checked
+            })
+          }}
+      /> 
+    </div>
+
+    <h5 style={{position:"absolute",top:"17%",left:"2%",zIndex:"10"}}>Initial date: </h5>
+
+    <div style={{position:"absolute",top:"24%",left:"2%",zIndex:"12"}}>
+      <DateTimePicker
+        returnValue={'start'}
+        onChange={(value) => this.setState({date_in: value})}
+        value={this.state.date_in}
+        maxDate={this.state.date_fin}
+      />
+    </div>
+
+    <h5 style={{position:'absolute',top:"26%",left:"2%",zIndex:"10"}}>Final date: </h5>
+
+    <div style={{position:'absolute',top:"33%",left:"2%",zIndex:"11"}}>
+      <DateTimePicker
+        returnValue={'start'}
+        onChange={(value) => this.setState({date_fin: value})}
+        value={this.state.date_fin}
+        minDate={this.state.date_in}
+      />
+    </div>
+
+    <h5 style={{position:"absolute",top:"39%",left:"2%",zIndex:"10"}}>Bus number: </h5>
+
+    <div style={{position:"absolute",top:"46%",left:"2%",zIndex:"10",width:"25%"}}>
+      <Select 
+        options={this.state.Opt} 
+        //isMulti
+        isDisabled={false}
+        value={this.state.ID}
+        onChange={(value)=>{
+          this.setState({
+            ID:value
+          });
+        }}
+      />
+    </div>
+
+    </SlidingPanel>
+
+    <div>
+    <LoadScript
+       googleMapsApiKey={API_KEY}>
+
+    <GoogleMap 
+    mapContainerStyle={mapContainerStyle} 
+    zoom={8} center={center} 
+    options={options}
+    > 
+
+    <Polyline
+      visible={this.state.sw_history}
+      path={this.state.history}
+      options={{
+        strokeColor:'#301586',
+      }}
+    />
+    
+    <Marker
+      position={this.state.coord}
+      icon={"/lorry-512_2.0.png"}
+      visible={this.state.sw_tag}
+    />
+
+    <Marker
+      position={this.state.history[(this.state.history.length-1)]}
+      icon={"/mapa.svg"}
+      visible={this.state.sw_his_tag}
+    />
+
+    <Marker
+      position={this.state.history[0]}
+      icon={"/ubicacion.svg"}
+      visible={this.state.sw_his_tag}
+    />
+
+    </GoogleMap>
+
+    </LoadScript>
+    </div>
+
+    </div>
+
+  );
+}
+}
+export default App;
---
+++ gps_web/udp-api/routes/testAPI.js
@@ -0,0 +1,189 @@
+
+var express = require("express");
+var mysql = require('mysql');
+const stringify= require("querystring");
+var router = express.Router();
+var udp = require('dgram');
+var ENV = require('./env.json');
+const wait= require("@testing-library/react");
+const { resolve } = require("path");
+const { rejects } = require("assert");
+
+global.id=0;
+
+const HOST=ENV.HOST;
+const USER=ENV.USER;
+const PASSWORD=ENV.PASSWORD;
+const DATA=ENV.DATA;
+
+var con = mysql.createConnection({
+  host: HOST,
+  user: USER,
+  password: PASSWORD,
+  database: DATA
+});
+
+function connectDatabase(){
+
+  var last_row=0;
+
+  con.query(("SELECT * FROM truckdata WHERE id = (SELECT MAX(id) FROM truckdata)"), function (err, result) {
+
+    if (err) throw err;
+
+    last_row=result;
+    last_row=JSON.parse(JSON.stringify(last_row));
+    last_row=last_row[0];
+
+    try{
+      console.log("\n");
+      console.log("Last table ID obtained");
+      global.id=last_row.id;
+      console.log(global.id);
+      console.log("\n");
+    }catch(err){
+      console.log("\n");
+      console.log("Last table ID obtained");
+      global.id=0;
+      console.log(global.id);
+      console.log("\n");
+    }
+
+  });
+
+}
+
+function Updatetable(message){
+
+  return new Promise((resolve,reject)=>{
+    var buff_sql=message.split("\n");
+    (global.id)=(global.id)+1;
+    var i_str=(global.id).toString();
+
+    var sql = ("INSERT INTO truckdata (id,lat, lng, alt, timegps, truck) VALUES (").concat(i_str,",",buff_sql[1],"," ,buff_sql[0],",", buff_sql[2],",",buff_sql[3],",",buff_sql[4],')');
+
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      return err ? reject(err): resolve("1 record inserted");
+    });
+
+  });
+
+}
+
+function Getusers(){
+  
+  return new Promise((resolve,reject)=>{
+    con.query(("SELECT DISTINCT truck FROM truckdata;"),
+      function(err,result){
+        var users=JSON.parse(JSON.stringify(result));
+        return err ? reject(err): resolve(users);
+      }
+    );
+  });
+
+}
+
+function GetLast(truck){
+
+  return new Promise((resolve,reject)=>{
+    con.query((("SELECT lat,lng,alt,timegps FROM truckdata WHERE truck = ").concat((truck).toString()," ORDER BY id desc limit 1")),
+      function Getlasmessage(err,result){
+        var lastposition=JSON.parse(JSON.stringify(result));
+        return err ? reject(err): resolve(lastposition);
+      }
+    ); 
+  });
+
+}
+
+function Gethistory(truck,time_in,time_fin){
+  return new Promise((resolve,reject)=>{
+    con.query((("SELECT lat,lng FROM truckdata WHERE truck = ").concat((truck).toString()," AND timegps BETWEEN ",(time_in).toString(),' and ',(time_fin).toString())),
+      function(err,result){
+        if(err){throw err};
+        var history=JSON.parse(JSON.stringify(result));
+        return err ? reject(err): resolve(history);
+      }
+    );
+  });
+
+}
+
+var server= udp.createSocket('udp4');
+server.bind(5000);
+
+server.on('listening',function(){
+  try{
+    address=server.address();
+    port= address.port;
+    ipaddr = address.address;
+    console.log('Server is listening at port ' + port);
+    console.log('Server ip :' + ipaddr);
+    connectDatabase()
+  }catch(error){
+    console.error();
+  }
+});
+
+server.on('message',function(msg,info){
+  
+  try{
+    var message=msg.toString();
+    async function Update(){
+      console.log(await Updatetable(message));
+    }
+    Update();
+  }catch(error){
+    console.error();
+  }
+
+});
+
+router.get("/users", function(req, res, next) {
+
+  try{
+    async function User(){
+      var users= await Getusers();
+      res.json(users);
+    }
+    User();
+  }catch(error){
+    console.error();
+  }
+  
+});
+
+router.post("/history", function(req, res, next) {
+
+  try{
+    var time_in=req.body.timestamp_in;
+    var time_fin=req.body.timestamp_fin;
+    var truck=req.body.ID;
+    async function History(){
+      var history= await Gethistory(truck,time_in,time_fin);
+      res.json(history);
+    }
+    History();
+  }catch(error){
+    console.error();
+  }
+  
+});
+
+router.post("/last", function(req,res, next){
+
+  try{
+    var truck=req.body.ID; 
+    async function Last(){
+      var last=await GetLast(truck);
+      res.json(last);
+    }
+    Last();
+  }catch(error){
+    console.error();
+  }
+  
+});
+
+module.exports = router;
