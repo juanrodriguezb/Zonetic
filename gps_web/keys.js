e84984fe02c9b99c1ab0e615695a5f54ff1c923e
juanrodriguezb <68094743+juanrodriguezb@users.noreply.github.com>
2020-11-05 01:28:51 -0500

    Delete keys.js

--- gps_web/keys.js
+++
@@ -1,9 +0,0 @@
-// keys.js
-
-module.exports = {
-    REACT_APP_MAPS_API: process.env.REACT_APP_MAPS_API,
-    REACT_APP_API_URL_1: process.env.REACT_APP_API_URL_1,
-    REACT_APP_API_URL_2: process.env.REACT_APP_API_URL_2,
-    REACT_APP_API_URL_3: process.env.REACT_APP_API_URL_3,
-
-};
